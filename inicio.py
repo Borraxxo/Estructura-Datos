5482d10b11fb72c3a720d213d4931a6e3f68bb72
yefersonstiv2014 <yefersonstiv2014@gmail.com>
2024-08-10 17:24:12 +0000

    nuevo codigo

--- inicio.py
+++ inicio.py
@@ -1,2 +1,8 @@
 edad = 22
 print(edad)
+edad = int(input("Introduce tu edad: "))  # Ask the user for their age
+
+if edad < 18:
+    print("No puedes entrar.")  # Message if they are under 18
+else:
+    print("Puedes entrar.")   # Message if they are 18 or older
